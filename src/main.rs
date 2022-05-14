507b240062bafb51600561e119a7d3a3fabf24f3
Taylor Fahlman <tfahlman@redhat.com>
2022-05-14 09:07:10 -0400

    Implement Endpoint struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+extern crate serde_yaml;
+
+#[macro_use]
+extern crate serde_derive;
+
+
+struct ReachabilityConfig {
+    Endpoints: Vec<Endpoint>
+}
+
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all="camelCase")]
+struct Endpoint {
+
+    host: String,
+    ports: Vec<i32>,
+    tls_disabled: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    let endpoint = Endpoint{
+        host: String::from("quay.io"),
+        ports: vec![80,443],
+        tls_disabled: false,
+    };
+
+    let yaml = serde_yaml::to_string(&endpoint);   
+    println!("{}", yaml.unwrap()) 
+    
 }
