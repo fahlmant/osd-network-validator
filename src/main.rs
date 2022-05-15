04bef3b00d1f8f4e88e7d5b12292d9a6c88ccea3
Taylor Fahlman <tfahlman@redhat.com>
2022-05-15 15:03:10 -0400

    Setup basic logic and http client for tests

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,81 @@
 extern crate serde_yaml;
 
+use std::fs;
+use std::time::Duration;
+
 #[macro_use]
 extern crate serde_derive;
 
-
-struct ReachabilityConfig {
-    Endpoints: Vec<Endpoint>
-}
-
-
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(rename_all="camelCase")]
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+// Represents an endpoint configuration to test connection to
 struct Endpoint {
-
     host: String,
     ports: Vec<i32>,
-    tls_disabled: bool,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    tls_disabled: Option<bool>,
+}
+
+#[derive(Debug, Deserialize)]
+// Represents a list of endpoint configurations
+struct ReachabilityConfig {
+    endpoints: Vec<Endpoint>,
+}
+
+impl ReachabilityConfig {
+    // Loads yaml from config file into the ReachabilityConfig
+    fn load_from_yaml(&mut self, file_path: String) {
+        // Read file into memory
+        let data = fs::read_to_string(file_path).expect("Unable to read file");
+        // Marshal the data into the ReachabilityConfig struct
+        let e = serde_yaml::from_str::<ReachabilityConfig>(&data).unwrap();
+        self.endpoints = e.endpoints
+    }
+
+    // Performs connectivity test for each endpoint
+    fn test_endpoints(self, timeout: Duration) {
+        for e in self.endpoints.iter() {
+            for p in e.ports.iter() {
+                // Validate that the endpoint is reachable for each host:port combination
+                validate_reachability(&e.host, p, e.tls_disabled, timeout);
+            }
+        }
+    }
 }
 
 fn main() {
 
-    let endpoint = Endpoint{
-        host: String::from("quay.io"),
-        ports: vec![80,443],
-        tls_disabled: false,
+    let timeout = Duration::from_secs(2);
+
+    // Init new ReachabilityConfig
+    let mut reachability_config = ReachabilityConfig {
+        endpoints: Vec::new(),
     };
 
-    let yaml = serde_yaml::to_string(&endpoint);   
-    println!("{}", yaml.unwrap()) 
+    reachability_config.load_from_yaml(String::from("config.yaml"));
+
+    reachability_config.test_endpoints(timeout);
+}
+
+// Attempts to establish a connection and communicate to the provided host:port combinationk
+fn validate_reachability(host: &String, port: &i32, tls_disabled: Option<bool>, timeout: Duration) {
+
+    // Setup the client builder with timoout provided
+    let mut client_builder = reqwest::Client::builder().timeout(timeout);
+
+    // If tls is explicitly disabled, then add option to accept invalid certs to client builder
+    // Do nothing if tls_disabled is false or missing
+    match tls_disabled {
+        Some(t) => if t {client_builder = client_builder.danger_accept_invalid_certs(true);}
+        None => {},
+    }
+
+    let client = client_builder.build();
     
+    match port {
+        80 => println!("{}:{} {:?}", host, port,tls_disabled),
+        443 => println!("{}:{} {:?}", host, port,tls_disabled),
+        22 => println!("{}:{} {:?}", host, port,tls_disabled),
+        _ => println!("{}:{} {:?}", host, port,tls_disabled),
+    }
 }
